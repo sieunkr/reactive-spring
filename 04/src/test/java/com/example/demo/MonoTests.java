e76f87d1a5e34330e5f5322be74d3d02ba6ac2fd
Eddy.Kim <sieunkr@gmail.com>
2019-01-13 13:53:15 +0900

    Mono Test 추가

--- 04/src/test/java/com/example/demo/MonoTests.java
+++ 04/src/test/java/com/example/demo/MonoTests.java
@@ -24,6 +24,8 @@ public class MonoTests {
 
         List<Signal<Integer>> signals = new ArrayList<>(4);
 
+        final Integer[] result = new Integer[1];
+
         Mono<Integer> mono = Mono.just(1).log()
                 /*
                 //.doOnEach(signals::add);
@@ -40,9 +42,43 @@ public class MonoTests {
                     System.out.println("Signal... : " + integerSignal);
                 });
 
+
+        mono.subscribe(integer -> result[0] = integer);
+
+
+        Assert.assertEquals(signals.size(), 2);
+        Assert.assertEquals(signals.get(0).getType().name(),"ON_NEXT");
+        Assert.assertEquals(signals.get(1).getType().name(),"ON_COMPLETE");
+
+        Assert.assertEquals(result[0].intValue(), 1);
+    }
+
+    @Test
+    public void test_mono_empty(){
+
+        List<Signal<Integer>> signals = new ArrayList<>(4);
+        Mono<Integer> mono = Mono.<Integer>empty()
+                .doOnEach(signals::add);
+
         mono.subscribe();
 
-
+        Assert.assertEquals(signals.size(), 1);
+        Assert.assertTrue(signals.get(0).isOnComplete());
     }
+
+
+    @Test
+    public void test_mono_null(){
+
+        String expected = null;
+        Mono<String> result = Mono.empty();
+
+        Assert.assertEquals(expected, result.block());
+    }
+
+
+    
+
+
 
 }
